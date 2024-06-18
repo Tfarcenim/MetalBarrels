ea22eb20337c5a962e2f4230f4ea4cbd586d24b9
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-06-18 12:11:54 -0400

    fix wrong mineable tag

--- src/main/java/tfar/metalbarrels/datagen/data/ModBlockTagsProvider.java
+++ src/main/java/tfar/metalbarrels/datagen/data/ModBlockTagsProvider.java
@@ -2,6 +2,7 @@ package tfar.metalbarrels.datagen.data;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -16,6 +17,9 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
 
     @Override
     protected void addTags() {
+        tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.COPPER_BARREL,ModBlocks.IRON_BARREL,ModBlocks.SILVER_BARREL,ModBlocks.GOLD_BARREL,ModBlocks.DIAMOND_BARREL,
+                ModBlocks.CRYSTAL_BARREL,ModBlocks.OBSIDIAN_BARREL,ModBlocks.NETHERITE_BARREL);
+
         tag(ModTags.Blocks.WOOD_BARRELS).add(Blocks.BARREL);
         tag(ModTags.Blocks.COPPER_BARRELS).add(ModBlocks.COPPER_BARREL);
         tag(ModTags.Blocks.IRON_BARRELS).add(ModBlocks.IRON_BARREL);
--- src/main/java/tfar/metalbarrels/init/ModBlocks.java
+++ src/main/java/tfar/metalbarrels/init/ModBlocks.java
@@ -8,8 +8,8 @@ import net.minecraft.world.level.material.Material;
 
 public class ModBlocks {
 
-    static Block.Properties metal = Block.Properties.of(Material.METAL).strength(1,6).sound(SoundType.METAL);
-    static Block.Properties obsidian = Block.Properties.of(Material.STONE).strength(2.5f,6000);
+    static Block.Properties metal = Block.Properties.of(Material.METAL).strength(1,6).sound(SoundType.METAL).requiresCorrectToolForDrops();
+    static Block.Properties obsidian = Block.Properties.of(Material.STONE).strength(2.5f,6000).requiresCorrectToolForDrops();
     
     public static final Block COPPER_BARREL = new MetalBarrelBlock(metal, MetalBarrelBlockEntityType.copper());
     public static final Block IRON_BARREL = new MetalBarrelBlock(metal, MetalBarrelBlockEntityType.iron());
